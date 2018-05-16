734c2f123a0a41ff52364d96ffc645d5c196a8ae
andrew <kalaz.bb@yahoo.com>
2018-05-17 12:33:13 +1400

    installed lowdb, added examples, done add app function

--- apps/db.js
+++ apps/db.js
@@ -1,2 +1,15 @@
 // TODO: DB using lowdb + admin panel with btoa encryotion and passport(cookies)
 // TODO: Installing lowdb and configuring
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('apps.json');
+const db = low(adapter);
+
+db.defaults({
+    apps: []
+}).write();
+
+module.exports.addApp = (what, name, category) => {
+    db.get(`apps.${category}`).push(what)
+}
---
+++ apps/example.js
@@ -0,0 +1,37 @@
+[{
+    'name': 'Eskimo',
+    'description': 'Eskimo the bot',
+    'apps': [{
+            'name': 'API',
+            'status': {
+                'type': 'success',
+                'text': 'Functional'
+            },
+            'description': 'Eskimo\'s API',
+            'fullDescription': 'This is Eskimo\'s API for website'
+        },
+        {
+            'name': 'Client',
+            'status': {
+                'type': 'success',
+                'text': 'Online'
+            },
+            'description': 'Eskimo it self',
+            'fullDescription': 'Eskimo Clinet, Discord.js'
+        }
+    ]
+},
+{
+    'name': 'Page',
+    'description': 'This page',
+    'apps': [{
+        'name': 'API',
+        'status': {
+            'type': 'success',
+            'text': 'functional'
+        },
+        'description': 'Page\'s API',
+        'fullDescription': 'This is this Page\'s API for apps'
+    }]
+}
+]
